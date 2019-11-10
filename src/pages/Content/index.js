5234ec85ddcf594fb371a51fefa776c70126dbfa
Gur Dotan <gdotan@salesforce.com>
2019-11-10 22:36:12 +0200

    Add some initial UI

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import rules from './diff-rules';
+import Toolbar from './components/toolbar';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 const CODE_ADDITION = '.blob-code-addition';
 const CODE_DELETION = '.blob-code-deletion';
+
+
+
+import { AppTheme, lightTheme } from '@datorama/app-components';
+import { ThemeProvider } from 'styled-components';
+
+const App = () => (
+  <AppTheme theme={lightTheme} provider={ThemeProvider}>
+    <Toolbar/>
+  </AppTheme>
+);
+
+
 
 
 function findCodeChangeNodes(file) {
@@ -34,13 +48,18 @@ function parseToFileList(file) {
 }
 
 const onlyFilesWithChanges = file => file.changes.length;
+let rendered = false;
 
 const powerDiff = () => {
-  // const container = document.querySelector('.pr-review-tools');
-  // const anchor = document.createElement('div');
-  // anchor.id = 'extension-root';
-  // container.insertBefore(anchor, container.childNodes[0]);
-  // ReactDOM.render(<Newtab/>, anchor);
+  if (!rendered) {
+    const container = document.querySelector('.pr-review-tools');
+    const anchor = document.createElement('div');
+    anchor.style.cssFloat = 'left'
+    anchor.id = 'extension-root';
+    container.insertBefore(anchor, container.childNodes[0]);
+    ReactDOM.render(<App/>, anchor);
+    rendered = true;
+  }
 
   [...document.querySelectorAll('.file')]
     .map(parseToFileList)
@@ -73,6 +92,6 @@ const start = () => {
 // observe();
 
 // Didn't work properly without a setTimeout for some reason
-setTimeout(start, 10000);
+setTimeout(start, 1000);
 
 

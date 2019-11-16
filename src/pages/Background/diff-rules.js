7ccd379cfd7291fabe3f34adc304395375bdc8d3
Gur Dotan <gdotan@salesforce.com>
2019-11-17 01:06:48 +0200

    Consolidate two diff rules

--- src/pages/Background/diff-rules.js
+++ src/pages/Background/diff-rules.js
@@ -1,11 +1,11 @@
 const rules = {
 	java: [
 		{
-			name    : 'Hide files only with import changes',
+			name    : 'Hide files only with import/package changes',
 			callback:( ({ el, changes }) => {
 				const importChangesOnly = changes
 					.map(node => node.text)
-					.reduce((acc, text) => acc && (text.startsWith('import ') || text === ''), true);
+					.reduce((acc, text) => acc && (text.startsWith('import ') || text.startsWith('package ') || text === ''), true);
 
 				if (importChangesOnly) {
 					const viewedCheckbox = el.querySelector('.js-reviewed-checkbox');
@@ -18,26 +18,7 @@ const rules = {
 					}
 				}
 			}).toString(),
-		},
-		{
-			name    : 'Hide files only with package name changes',
-			callback: (({ el, changes }) => {
-				const packageChangesOnly = changes
-					.map(node => node.text)
-					.reduce((acc, text) => acc && (text.startsWith('package ') || text === ''), true);
-
-				if (packageChangesOnly) {
-					const viewedCheckbox = el.querySelector('.js-reviewed-checkbox');
-
-					//
-					// viewedCheckbox exists only in PR pages, not commit pages
-					//
-					if (viewedCheckbox && !viewedCheckbox.checked) {
-						viewedCheckbox.click();
-					}
-				}
-			}).toString(),
-		},
+		}
 	],
 };
 
